abcc3e45033fad0881ea816c4249cc1ea5025c79
Edita-TheEksperts <edita.latifi@the-eksperts.com>
2025-01-19 00:26:14 +0100

    Update Servicesgrid.js

--- src/pages/Servicesgrid.js
+++ src/pages/Servicesgrid.js
@@ -9,7 +9,13 @@ function Servicegrid() {
 
   const columnsData = [
     {
-      title: "Alltagsbegleitung und Besorgungen",
+      title:(
+        <p
+          
+        >
+          Alltagsbegleitung<br></br> und Besorgungen​
+        </p>
+      ),
       items: [
         {
           heading: "- Begleitung zu Terminen",
@@ -34,7 +40,13 @@ function Servicegrid() {
       ],
     },
     {
-      title: "Freizeit und soziale Aktivitäten",
+      title: (
+        <p
+          
+        >
+          Freizeit und <br></br>soziale Aktivitäten​
+        </p>
+      ),
       items: [
         { heading: "- Gesellschaft leisten" },
         { heading: "- Gemeinsames Kochen" },
@@ -54,7 +66,14 @@ function Servicegrid() {
       ],
     },
     {
-      title: "Gesundheitsfürsorge",
+      title:(
+        <p
+          
+        >
+          Gesundheits<br />
+          fürsorge
+        </p>
+      ),
       items: [
         { heading: "- Körperliche Unterstützung" },
         { heading: "- Nahrungsaufnahme" },
@@ -64,7 +83,13 @@ function Servicegrid() {
       ],
     },
     {
-      title: "Haushaltshilfe und Wohnpflege",
+      title:(
+        <p
+          
+        >
+          Haushaltshilfe und<br></br> Wohnpflege​
+        </p>
+      ),
       items: [
         { heading: "- Hauswirtschaft" },
         { heading: "- Balkon und Blumenpflege" },
@@ -98,7 +123,7 @@ function Servicegrid() {
                   border-0 => heqim vijën rrethore
               */
               className={`
-                text-white text-lg font-bold px-6 py-4 relative
+                text-white text-[24px] leading-[24px] font-bold px-6 py-4 relative
                 rounded-[20px]
                 bg-[#B99B5F] border border-[#B99B5F]
                 lg:bg-[url('/images/1.png')] lg:bg-cover lg:bg-center
@@ -134,11 +159,11 @@ function Servicegrid() {
               <ul className="space-y-4">
                 {column.items.map((item, j) => (
                   <li key={j}>
-                    <span className="font-semibold text-[#B99B5F] text-[16px] leading-[25.6x]">
+                    <span className="font-[500] text-[#B99B5F] text-[16px] leading-[25.6x]">
                       {item.heading}
                     </span>
                     {item.subItems && (
-                      <ul className="pl-6 mt-2 text-[#5E5E5E] space-y-1 text-[14px] leading-[24px]">
+                      <ul className="pl-6 mt-2 text-[#5E5E5E] font-[300] space-y-1 text-[14px] leading-[24px]">
                         {item.subItems.map((subItem, k) => (
                           <li key={k}>• {subItem}</li>
                         ))}
