867f2c3d9768e88a30bbb264c41ee4a16c46b386
Edita-TheEksperts <edita.latifi@the-eksperts.com>
2025-01-20 13:19:59 +0100

    Update Form-Page-2.js

--- src/pages/Form-Page-2.js
+++ src/pages/Form-Page-2.js
@@ -11,7 +11,7 @@ export default function FormPage02() {
   return (
     <div className="bg-[#F1F1F1] flex flex-col items-center justify-start p-4">
       {/* Back Button and Logo */}
-      <section className="md:block hidden md:flex justify-center items-center pt-[25px] md:pt-[25px]">
+      <section className="lg:block hidden lg:flex justify-center items-center pt-[25px] md:pt-[25px]">
         <div className="absolute top-6 left-4 lg:left-[170px]">
           <Link href="/Form-Page-1">
             <button
@@ -57,7 +57,7 @@ export default function FormPage02() {
         </div>
       </section>
 
-      <section className='md:hidden block flex justify-center items-center '>
+      <section className='lg:hidden block flex justify-center items-center '>
         <div className=" mt-0">
           <div className="mb-[100px]">
             <svg xmlns="http://www.w3.org/2000/svg" width="131" height="64" viewBox="0 0 131 64" fill="none">
@@ -114,7 +114,7 @@ export default function FormPage02() {
 
       {/* Heading */}
       <h1
-        className="text-[#04436F] font-metropolis font-[700] text-[48px] leading-[55.2px] text-center md:mb-10 md:mt-[80px]"
+        className="lg:block hidden text-[#04436F] font-metropolis font-[600] lg:font-[700] text-[24px] lg:text-[48px] leading-[32px] lg:leading-[55.2px] text-center lg:mb-10 lg:mt-[80px]"
         style={{
           letterSpacing: "-0.96px", 
         }}
@@ -122,9 +122,18 @@ export default function FormPage02() {
         Vielen Dank! <br />
         Wir melden uns so schnell wie möglich
       </h1>
+      <h1
+        className="lg:hidden block text-[#04436F] font-metropolis font-[600] lg:font-[700] text-[24px] lg:text-[48px] leading-[32px] lg:leading-[55.2px] text-center lg:mb-10 lg:mt-[80px]"
+        style={{
+          letterSpacing: "-0.96px", 
+        }}
+      >
+        Vielen Dank! <br />
+        Wir melden uns so<br /> schnell wie möglich
+      </h1>
 
       {/* Section */}
-      <section className="md:block hidden md:flex flex-col gap-[40px] w-full max-w-[1234px] mb-[160px]">
+      <section className="lg:block hidden lg:flex flex-col gap-[40px] w-full max-w-[1234px] mb-[160px]">
         {/* Two Divs Side by Side */}
         <div className="flex gap-[40px]">
           {/* Left Box */}
@@ -193,7 +202,7 @@ Du betreust fest zugewiesene Privatkunden in deiner Region. Je nach Bedarf kann 
         </div>
       </section>
 
-      <section className="md:hidden block mt-[50px] p-4">
+      <section className="lg:hidden block mt-[50px] p-4">
 
       <div className="flex flex-col justify-center items-center p-[2px] mx-auto ">
           <h2
@@ -258,9 +267,9 @@ Du betreust fest zugewiesene Privatkunden in deiner Region. Je nach Bedarf kann 
       </section>
 
       {/* Footer Section */}
-      <div className="flex flex-col items-center mt-[160px] md:mt-0 gap-4">
+      <div className="flex flex-col items-center mt-[160px] gap-4">
         <h1
-          className="text-[#04436F] font-metropolis font-[600] md:font-[700] text-[24px] md:text-[55px] leading-[55.2px] text-center"
+          className="text-[#04436F] font-metropolis font-[600] lg:font-[700] text-[24px] lg:text-[55px] leading-[55.2px] text-center"
           style={{
             letterSpacing: "-0.96px",
           }}
@@ -270,20 +279,20 @@ Du betreust fest zugewiesene Privatkunden in deiner Region. Je nach Bedarf kann 
         <input
           type="text"
           placeholder="Deine Fragen eintippen"
-          className="flex flex-col items-start self-stretch text-center border-[1px] rounded-[20px] border-[#B7B6BA6A] p-[20px] placeholder:text-[#04436F] text-[#04436F] font-metropolis text-[20px] font-light leading-[26px] w-full md:w-[826px] bg-transparent focus:outline-none"
+          className="flex flex-col items-start self-stretch text-center border-[1px] rounded-[20px] border-[#B7B6BA6A] p-[20px] placeholder:text-[#04436F] text-[#04436F] font-metropolis text-[20px] font-light leading-[26px] w-full lg:w-[826px] bg-transparent focus:outline-none"
           />
 
 <div className="container flex justify-center items-center">
       {!isSubmitted ? (
         <button
           type="button"
-          className="bg-[#04436F] text-[#F5F5F5] font-metropolis font-bold text-[24px] md:text-[32px] leading-[21.6px] rounded-[8px] md:rounded-full px-8 py-4 mb-[150px]"
+          className="bg-[#04436F] text-[#F5F5F5] font-metropolis font-bold text-[24px] lg:text-[32px] leading-[21.6px] rounded-[8px] lg:rounded-full px-8 py-4 mb-[150px]"
           onClick={handleClick} // Trigger state change on click
         >
           Senden
         </button>
       ) : (
-        <p className="text-[#04436F] font-metropolis font-[600] md:font-[700] text-[24px] md:text-[50px] leading-[26.2px] md:leading-[50.2px] text-center">
+        <p className="text-[#04436F] font-metropolis font-[600] lg:font-[700] text-[24px] lg:text-[50px] leading-[26.2px] lg:leading-[50.2px] text-center">
           Vielen Dank - Wir melden uns so schnell wie möglich!
         </p>
       )}
