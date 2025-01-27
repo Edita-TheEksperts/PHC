9de2ad87f7e92922fb8984a2f28a62756ef362eb
Edita-TheEksperts <edita.latifi@the-eksperts.com>
2025-01-27 14:25:41 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import { FormProvider } from "./FormContext";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <FormProvider>
+      <Component {...pageProps} />
+    </FormProvider>
+  );
 }
+
+export default MyApp;
+
