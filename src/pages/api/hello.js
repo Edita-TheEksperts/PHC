17d210a5302d0375051da694139309eaeabe5074
Edita-TheEksperts <edita.latifi@the-eksperts.com>
2025-01-27 14:29:40 +0100

    Update hello.js

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import React from 'react';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+export default function HomePage() {
+  return <h1>Hello, World!</h1>;
 }
+
